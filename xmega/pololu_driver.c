c62847e354e608bf33bf0372bb3ca1ce121f4b12
Austin Baylis <austinbaylis@gmail.com>
2014-10-26 13:37:02 -0400

    inital fix to xmega code

--- xmega/pololu_driver.c
+++ xmega/pololu_driver.c
@@ -20,29 +20,29 @@ pololu_t pololu_left = {
 	};
 	
 pololu_t pololu_right = {
-		.PORT = &PORTE,
+		.PORT = &PORTC,
 		.TC0 = &TCC0,
 		.TC1 = &TCC1
 	};
 
 void pololuInit(void){
 	// LEFT MOTOR
-	PORTE.DIR = 0xCF;							// Set PORT directions
-	PORTE.OUT = 0x0E;							// Set inital outputs
+	pololu_left.PORT->DIR = 0xCF;					// Set PORT directions
+	pololu_left.PORT->OUT = 0x0E;								// Set inital outputs
 		
-	TCE0.CTRLA |= TC_CLKSEL_DIV1_gc;				// Set clock divider to 64
-	TCE0.CTRLB |= 0x10 | TC_WGMODE_SINGLESLOPE_gc;  // Enable single slope PWM. Enable OC A.
-	TCE0.PER = PERIOD;    							// PER = (40kHz*2MHz)/1 = 50
-	TCE0.CCA = MAGNITUDE;
+	pololu_left.TC0->CTRLA |= TC_CLKSEL_DIV1_gc;				// Set clock divider to 64
+	pololu_left.TC0->CTRLB |= 0x10 | TC_WGMODE_SINGLESLOPE_gc;  // Enable single slope PWM. Enable OC A.
+	pololu_left.TC0->PER = PERIOD;    							// PER = (40kHz*2MHz)/1 = 50
+	pololu_left.TC0->CCA = MAGNITUDE;
 	
 	// RIGHT MOTOR
-	PORTC.DIR = 0xCF;							// Set PORT directions
-	PORTC.OUT = 0x0E;							// Set inital outputs
+	pololu_right.PORT->DIR = 0xCF;							// Set PORT directions
+	pololu_right.PORT->OUT = 0x0E;							// Set inital outputs
 		
-	TCC0.CTRLA |= TC_CLKSEL_DIV1_gc;				// Set clock divider to 64
-	TCC0.CTRLB |= 0x10 | TC_WGMODE_SINGLESLOPE_gc;  // Enable single slope PWM. Enable OC A.
-	TCC0.PER = PERIOD;    							// PER = (40kHz*2MHz)/1 = 50
-	TCC0.CCA = MAGNITUDE;
+	pololu_right.TC0->CTRLA |= TC_CLKSEL_DIV1_gc;				// Set clock divider to 64
+	pololu_right.TC0->CTRLB |= 0x10 | TC_WGMODE_SINGLESLOPE_gc;  // Enable single slope PWM. Enable OC A.
+	pololu_right.TC0->PER = PERIOD;    							// PER = (40kHz*2MHz)/1 = 50
+	pololu_right.TC0->CCA = MAGNITUDE;
 }
 
  void pololuDrive(pololu_t *pololu, int8_t percent){
@@ -50,12 +50,3 @@ void pololuInit(void){
 	// Should probably write some respectible code
 	pololu->TC0->CCA = (MAGNITUDE + (MAGNITUDE * ((double)percent/100)));
 }
-
-// int main (void)
-// {	
-// 	pololuInit();
-// 	pololuDrive(&pololu_left, 50);
-// 	while(1){
-// 		asm("nop");
-// 	}
-// }
