f2562fce46fb4998f7281958200ca3c7ef02691a
Austin Baylis <austinbaylis@gmail.com>
2014-10-28 13:38:00 -0400

    updated atmel code

--- xmega/pololu_driver.h
+++ xmega/pololu_driver.h
@@ -10,6 +10,19 @@
 
 #include <asf.h>
 
+#define CW_DIR   0 /* Clockwise direction. */
+#define CCW_DIR  1 /* Counter Clockwise direction. */
+#define CLOCK_DIV_bm  TC_CLKSEL_DIV64_gc
+#define CLOCK_DIV     64
+
+/*! This macro return the value of the capture register.
+ *
+ * \param  _tc   The Timer/Counter to get the capture value from.
+ */
+#define GetCaptureValue(_tc)  ( _tc->CCA )
+
+
+
 typedef struct {
 	PORT_t *PORT;
 	TC0_t * TC0;
@@ -21,5 +34,36 @@ extern pololu_t pololu_right;
 
 void pololuInit(void);
 void pololuDrive(pololu_t *pololu, int8_t percent);
+
+bool QDEC_Total_Setup(PORT_t * qPort,
+                      uint8_t qPin,
+                      bool invIO,
+                      uint8_t qEvMux,
+                      EVSYS_CHMUX_t qPinInput,
+                      bool useIndex,
+                      EVSYS_QDIRM_t qIndexState,
+                      TC0_t * qTimer,
+                      TC_EVSEL_t qEventChannel,
+                      uint16_t lineCount);
+
+bool QDEC_Port_Setup(PORT_t * qPort, uint8_t qPin, bool useIndex, bool invIO);
+
+bool QDEC_EVSYS_Setup(uint8_t qEvMux,
+                      EVSYS_CHMUX_t qPinInput,
+                      bool useIndex,
+                      EVSYS_QDIRM_t qIndexState );
+
+void QDEC_TC_Dec_Setup(TC0_t * qTimer,
+                       TC_EVSEL_t qEventChannel,
+                       uint16_t lineCount);
+
+void QDEC_TC_Freq_Setup(TC1_t * qTimer,
+                        TC_EVSEL_t qEventChannel,
+                        EVSYS_CHMUX_t qPinInput,
+                        TC_CLKSEL_t clksel);
+
+uint8_t QDEC_Get_Direction(TC0_t * qTimer);
+
+uint16_t QDEC_Get_Rpm(TC1_t *TC1);
 
 #endif // POLOLU_DRIVER_H_
