c1b42d070bc0c060ed8f4feff7a613504b5601b0
Austin Baylis <austinbaylis@gmail.com>
2014-11-10 16:04:52 -0800

    Add milaur_launch and changed navigation to use OpenCV. Still doesn't work for shit thou

--- ros/milaur_navigation/src/navigation.py
+++ ros/milaur_navigation/src/navigation.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from __future__ import division
+## OpenCV
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
 ## Math
 import numpy as np
 import math
@@ -10,73 +13,57 @@ import rospy
 ## Ros msgs
 from std_msgs.msg import Header, Int16, Bool, String, Float64
 from geometry_msgs.msg import Pose, PoseStamped, Twist, TwistStamped, Vector3
-from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Image
 
 
 class Navigator(object):
-    _min_angle = np.radians(-270/2)
-    _max_angle = np.radians(270/2)
-    _angular_resolution = np.radians(0.351625)
-    _avoidance_range = 2
-
-    _min_clearance = 1.5
-
     def __init__(self):
         rospy.init_node('navigation')
-        self.lidar_listener = rospy.Subscriber('/scan', LaserScan, self.analyze_scan)
+        self.logitech_listener = rospy.Subscriber('milaur/image_raw', Image, self.process_image)
         self.direction_pub = rospy.Publisher('milaur/angle_error', Float64)
 
-        #rospy.set_param("/hokuyo_node/min_ang", Float64(self._min_angle))
-        #rospy.set_param("/hokuyo_node/max_ang", Float64(self._max_angle))
+        # create ROS to CV bridge
+        self.bridge = CvBridge()
+
+	# define range for pink color in BGR
+        self.lower_pink = np.array([190, 29, 220], dtype=np.uint8)
+        self.uopoper_pink = np.array([220, 10, 250], dtype=np.uint8)
+
+        cv2.namedWindow("Image Window", 1)
+
         
-    def analyze_scan(self, msg):
-        # Pasted from the init
-        lidar_scan = msg.ranges
+    def process_image(self, msg):
+        # Get raw image from msg
+        try:
+            raw_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        except CvBridgeError, e:
+            print e
 
-        groups = self.find_available_ranges(lidar_scan)
+        # Convert BGR to HSV
+        hsv = cv2.cvtVolot(raw_img, cv2.COLOR_BGR2HSV)
 
-        # Playing with lidar data
-        #rospy.loginfo(lidar_scan[int(len(lidar_scan)/2)])
+        # Threshold HSV image for pink
+        mask = cv2.inRange(hsv, self.lower_pink, self.upper_pink)
 
-        if len(groups) != 0:
-            self.send_target_angle(min(groups, key = lambda x: abs(x)))
-        else:
-            self.send_target_angle(-15)
+        # Bitwise-AND mask and original image
+        res = cv2.bitwise_and(raw_img, raw_img, mask= mask)
+
+        cv2.imshow('Image Window', raw_img)
+        #cv2.imshow('mask', mask)
+        #cv2.imshow('res', res)
+
+        #if len(groups) != 0:
+        #    self.send_target_angle(min(groups, key = lambda x: abs(x)))
+        #else:
+        #    self.send_target_angle(-15)
+
 
     def send_target_angle(self, angle):
         '''Added to enable ros publishing of angle error'''
         self.direction_pub.publish(Float64(angle))
-             
-    def find_available_ranges(self, ranges):
-        '''Generates list of possible openings and their respective angles'''
-        groups = []
-        start = -1
-        end = -1
-        middle = len(ranges)/2
-        # Loops through the lidar data and finds openings.
-        #  IF there is nothing infront of me and I already have a start point
-        #    go ahead a shift my end point down
-        #  ELSE IF there is nothing infront of me, but I do not have a start point
-        #    set a start points
-        #  ELSE IF if I have a start, end, and there is something infront of me
-        #    find how wide the opening is. If it is wide enough, store the angle and
-        #     the width of the opening into the groups list.
-        for index, _range in enumerate(ranges):
-            if start >= 0 and _range > self._avoidance_range:
-                end = index
-            elif _range > self._avoidance_range:
-                start = index
-            elif start >= 0 and end >= 0:
-                theta = np.radians((end - start) * self._angular_resolution)
-                cord = np.sqrt(ranges[start]**2 + ranges[end]**2 + 2 * ranges[start] * ranges[end] * np.cos(theta))
-                if cord > self._min_clearance:
-                    error = ((end - ((end - start) / 2)) - middle) * self._angular_resolution
-                    groups.append(error)
-                start = -1
-                end = -1
-        return groups
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     navigator_node = Navigator()
     rospy.spin()     
 
